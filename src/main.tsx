9ae82073b9b2098c0f868d645374f6e8b8e8b47d
stosh2115 <stosh09@hotmail.com>
2024-04-03 15:44:14 -0700

    Theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider  } from '@mui/material/styles';
 
 
 import { About, Home, Projects } from './components';
 import './index.css'
+import { theme } from './Theme/themes';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home title= {"Morgan McIntosh"}/>} />
-        <Route path='/About' element={<About />} />
-        <Route path='/Projects' element={<Projects />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme = { theme }>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home title= {"Morgan McIntosh"}/>} />
+          <Route path='/About' element={<About />} />
+          <Route path='/Projects' element={<Projects />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>,
 )
